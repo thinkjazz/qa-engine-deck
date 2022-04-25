ebd5d7cafab470a8c54905363acbc97dd18547a9
Bragin Boris <bragin.boris@ya.ru>
2022-04-25 23:27:54 +0300

    Add some docs in dirs

---
+++ src/globals.ts
@@ -0,0 +1,6 @@
+/**
+ * Use this file to register any variables or functions that should be available globally
+ * ideally you should make it available via the window object
+ * as well as the Vue prototype for access throughout the app
+ * (register globals with care, only when it makes since to be accessible app wide)
+ */
