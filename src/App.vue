6665a4098569aaac4652338b4b4ba44cfac8a6f3
Bragin Boris <bragin.boris@ya.ru>
2022-04-27 00:56:54 +0300

    add landing section

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import { defineCustomElement } from 'vue'
 import TheNavbar from './components/TheNavbar.vue'
-import SideBar from './layouts/SideBar.vue'
 import CardGrid from './components/CardGrid.vue'
+
+import SideBar from './layouts/SideBar.vue'
+import Section from './layouts/Section.vue'
+
+// import TheFooter from './components/TheFooter.vue'
+
 
 </script>
 <template class="h-full">
 
-<TheNavbar />
-  <CardGrid />
-    <!-- <div class="flex-1">
- <SideBar />
 
+<Section/>
+
+    <!-- <div class="flex-1">
+<TheNavbar />
+ <SideBar />
+     <CardGrid />
     </div> -->
 </template>
 
