923f3c03820cab2a2af4721b29d480118198051f
Bragin Boris <bragin.boris@ya.ru>
2022-05-05 19:59:01 +0300

    added: AboutSection component

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,13 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="relative bg-emerald-800">
+    <div class="absolute inset-0">
+      <img class="object-cover w-full h-full" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100" alt="" />
+      <div class="absolute inset-0 bg-emerald-800 mix-blend-multiply" aria-hidden="true" />
+    </div>
+    <div class="relative px-4 py-24 mx-auto max-w-7xl sm:py-32 sm:px-6 lg:px-8">
+      <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">О школе</h1>
+      <p class="max-w-3xl mt-6 text-xl text-emerald-100">Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.</p>
+    </div>
+  </div>
+</template>
