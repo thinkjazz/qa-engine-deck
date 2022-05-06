279f32851bb94f12d8e28e0e970e141f852d171b
Bragin Boris <bragin.boris@ya.ru>
2022-05-07 00:02:01 +0300

    Added: ThePopularStack

---
+++ src/components/ThePopularStack.vue
@@ -0,0 +1,28 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <p class="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">Научим разрабатывать автотесты и выстраивать процессы в востребованном стеке</p>
+      <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img class="max-h-24" src="https://starchenkov.pro/qa-guru/img/skills/Intelij_IDEA.svg" alt="Workcation" />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img class="max-h-24" src="https://starchenkov.pro/qa-guru/img/skills/Java.svg" alt="Mirage" />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img class="max-h-24" src="https://starchenkov.pro/qa-guru/img/skills/Selenium.svg" alt="Tuple" />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img class="max-h-24" src="https://starchenkov.pro/qa-guru/img/skills/Selenide.svg" alt="Laravel" />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img class="max-h-24" src="https://starchenkov.pro/qa-guru/img/skills/Selenoid.svg" alt="StaticKit" />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img class="max-h-24" src="https://starchenkov.pro/qa-guru/img/skills/Allure_Report.svg" alt="Statamic" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
